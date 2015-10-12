88a12d98fdcb351ea63132032e4fdf2a88a317a8
mitchthorson <mthorson@usatoday.com>
2015-10-12 13:17:29 -0400

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,45 @@
+define([
+   "jquery",
+], function(jQuery) {
+    //set project data URL here
+    var dataURL = null;
+    //set project image path here
+    var imagePath = "";
+
+    //set project default share language here
+    var defaultShareLanguage = null;
+    var defaultShareImage = null
+
+    var isMobile, fb_app_id;
+    var isTablet = false;
+
+    var blnIframeEmbed = window != window.parent;
+
+    var staticInfo = JSON.parse(jQuery(".staticinfo").html());
+
+    if (staticInfo.platform == "desktop") {
+        isMobile = false;
+    } else {
+        isMobile = true;
+    }
+
+    if (isMobile === false) {
+        if (Modernizr.touch && window.innerWidth < 1100) {
+            isTablet = true;
+        }
+    }
+
+    fb_app_id = staticInfo.facebook.app_id;
+
+    return {
+        image_path: imagePath,
+        dataURL: dataURL,
+        staticInfo: staticInfo,
+        fb_app_id: fb_app_id,
+        isMobile: isMobile,
+        isTablet: isTablet,
+        defaultShareLanguage: defaultShareLanguage,
+        defaultShareImage: defaultShareImage,
+        isIframe: blnIframeEmbed
+    };
+});
