d7a3005538becfde137a5adaa88909decd7a16c8
mitchthorson <mthorson@usatoday.com>
2015-10-28 12:31:23 -0400

    add ticks to detail view

--- src/js/views/DetailView.js
+++ src/js/views/DetailView.js
@@ -2,12 +2,14 @@ define([
     "jquery",
     "underscore",
     "backbone",
+    "d3",
     "templates",
     "config",
     "views/CandidateDetailView"
-], function(jQuery, _, Backbone, templates, config, CandidateDetailView) {
+], function(jQuery, _, Backbone, d3, templates, config, CandidateDetailView) {
     return Backbone.View.extend({
         initialize: function(opts) {
+            this.listenTo(Backbone, "window:resize", this.drawTicks);
             this.data = opts.data;
             this.max_percent = 60;
             this.render();
@@ -27,7 +29,42 @@ define([
                 //save candidate detail view to detail view's array
                 _this.candidateDetailViews.push(candidateDetailView);
             });
+            this.drawTicks();
             return this;
+        },
+        drawTicks: function() {
+            this.$('.iapp-detail-ticks-chart').remove();
+            var margins = {
+                top: 15,
+                right: 50,
+                bottom: 15,
+                left: 50
+            };
+            var chartWidth = this.$el.outerWidth();
+            var chartHeight = this.$el.outerHeight();
+
+
+            var xScale = d3.scale.linear()
+                .domain([0, this.max_percent])
+                .range([margins.left, chartWidth - margins.right]);
+
+            var xAxis = d3.svg.axis()
+                .scale(xScale)
+                .orient("top")
+                .tickSize((chartHeight - (margins.top + margins.bottom)), 0, 0)
+                .tickPadding(5)
+                .tickFormat(function(d) {return d + "%";})
+                .ticks((this.max_percent/10) + 1);
+
+            var chartElement = d3.select(this.el).append('svg')
+                .attr("height", chartHeight)
+                .attr("width", chartWidth)
+                .attr("class", "iapp-detail-ticks-chart");
+
+            chartElement.append("g")
+                .attr("class", "detail-grid")
+                .attr("transform", "translate(0," + chartHeight + ")")
+                .call(xAxis);
         },
         update: function(newData) {
             var _this = this;
