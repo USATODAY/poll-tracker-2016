ef8a97c7c004b56f3225da782bfa32f6f75e4bf4
mitchthorson <mitch.thorson@gmail.com>
2016-02-18 18:52:38 -0500

    update local data files

--- src/js/config/races.js
+++ src/js/config/races.js
@@ -1,6 +1,6 @@
 define([], function() {
         return {
-            "republican": ["US", "NH", "IA", "SC", "FL"],
-            "democrat": ["US", "NH", "IA", "SC", "FL", ]
+            "republican": ["US", "NH", "IA", "SC", "FL", "NV", "GA", "TX", "MI", "NC"],
+            "democrat": ["US", "NH", "IA", "SC", "FL", "NV", "GA", "TX", "MI", "NC"]
         };
 });
