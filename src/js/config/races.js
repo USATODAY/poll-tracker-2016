1dca801f41bc20a1c80a5d160acba9cc21d8898f
mitchthorson <mitch.thorson@gmail.com>
2016-04-19 10:04:15 -0400

    add NY

--- src/js/config/races.js
+++ src/js/config/races.js
@@ -1,6 +1,6 @@
 define([], function() {
         return {
-            "republican": ["US", "NH", "IA", "SC", "FL", "NV", "GA", "TX", "MI", "NC", "OH", "IL"],
-            "democrat": ["US", "NH", "IA", "SC", "FL", "NV", "GA", "TX", "MI", "NC", "OH", "IL"]
+            "republican": ["US", "NH", "IA", "SC", "FL", "NV", "GA", "TX", "MI", "NC", "OH", "IL", "NY"],
+            "democrat": ["US", "NH", "IA", "SC", "FL", "NV", "GA", "TX", "MI", "NC", "OH", "IL", "NY"]
         };
 });
