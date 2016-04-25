c98a07af240813abf6387d15901ee315e0c46e92
mitchthorson <mitch.thorson@gmail.com>
2016-04-25 10:50:42 -0400

    add PA MD CT IN and CA

--- src/js/config/races.js
+++ src/js/config/races.js
@@ -1,6 +1,6 @@
 define([], function() {
         return {
-            "republican": ["US", "NH", "IA", "SC", "FL", "NV", "GA", "TX", "MI", "NC", "OH", "IL", "NY"],
-            "democrat": ["US", "NH", "IA", "SC", "FL", "NV", "GA", "TX", "MI", "NC", "OH", "IL", "NY"]
+            "republican": ["US", "NH", "IA", "SC", "FL", "NV", "GA", "TX", "MI", "NC", "OH", "IL", "NY", "PA", "MD", "CT", "IN", "CA"],
+            "democrat": ["US", "NH", "IA", "SC", "FL", "NV", "GA", "TX", "MI", "NC", "OH", "IL", "NY", "PA", "MD", "CT", "IN", "CA"]
         };
 });
